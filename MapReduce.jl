96a5f0acd1d4869d9d751fbe6d5a54142a116d54
bredaUgent <bram.breda@ugent.be>
2022-12-01 23:02:18 +0100

    mapreduction with old simd reduce macros

--- MapReduce.jl
+++ MapReduce.jl
@@ -1,10 +1,60 @@
-function MapReduce(m, A::AbstractGPUArray)
+using CUDA
+
+function MyMapReduce(A)
+    idx = threadIdx().x + (blockIdx().x - 1) * blockDim().x
+    mem = @cuStaticSharedMem(Float64, 10)
+    
+    val = 0
+    offset = 0
+
+    """
+    Eerst reduceren we op grote schaal we reduceren van de grote array naar een array die een 
+    grote van gridDim().x * blockDim().x
+    """
 
 
+    @inbounds while idx + offset <= length(A)
 
+        val = val + A[idx + offset]
+        offset += gridDim().x * blockDim().x
+
+    end
+
+    @cuprintln(idx, " ", val)
+    mem[idx] = val
+    """
+        probleem is hier dat shared memory enkel shared op basis van een block
+    """
+    sync_threads()
+
+    if idx == 1
+        for i in 1:10
+            @cuprintln( mem[i])
+        end
+    end
+
+    val = 0
+    if idx == 1
+        for i in 1:10
+            val += mem[i]
+        end
+        @cuprintln(val)
+    end
+    
+    return
 
 end
 
-function MapReduce_(A::CuArray)
+function main()
+    A = CUDA.rand(20)
 
+    print(A)
+
+    w = reduce(+, A)
+    println(w)
+
+    @cuda threads=5 blocks=2 MyMapReduce(A)
+3
 end
+
+main()
--- MyMapReduction.jl
+++ MyMapReduction.jl
@@ -1,29 +1,108 @@
-mutable struct MyGPUArray{T,N,B} <: AbstractGPUArray{T,N}
-    storage::Union{Nothing,ArrayStorage{B}}
-  
-    maxsize::Int  # maximum data size; excluding any selector bytes
-    offset::Int   # offset of the data in the buffer, in number of elements
-  
-    dims::Dims{N}
-  
-    function CuArray{T,N,B}(::UndefInitializer, dims::Dims{N}) where {T,N,B}
-      check_eltype(T)
-      maxsize = prod(dims) * sizeof(T)
-      bufsize = if isbitstype(T)
-        maxsize
-      else # isbitsunion etc
-        # type tag array past the data
-        maxsize + prod(dims)
-      end
-      buf = alloc(B, bufsize)
-      storage = ArrayStorage(buf, 1)
-      obj = new{T,N,B}(storage, maxsize, 0, dims)
-      finalizer(unsafe_finalize!, obj)
+using GPUArrays
+using CUDA
+import CUDA: @device_override
+using KernelAbstractions
+using CUDAKernels
+
+
+
+GPUArrays.mapreducedim!(f::F, op::OP, R::AnyCuArray{T}, A::Union{AbstractArray,Broadcast.Broadcasted}; 
+    init=nothing) where {F, OP, T} = 
+    invoke(mymapreducedim, Tuple{AnyCuArray{T}, Union{AbstractArray,Broadcast.Broadcasted}}, R, A)
+
+function reduce_SIMD(val)
+    offset = 0x00000001
+    while offset < @SIMDsize()
+
+        val += @SIMDreduce(0xffffffff, val, offset)
+        offset <<= 1
     end
-  
-    function CuArray{T,N}(storage::ArrayStorage{B}, dims::Dims{N};
-                          maxsize::Int=prod(dims) * sizeof(T), offset::Int=0) where {T,N,B}
-      check_eltype(T)
-      return new{T,N,B}(storage, maxsize, offset, dims)
+
+    return val
+end
+
+function reduce_group(val::T) where T
+    shared = @localmem T (32,1)
+
+    Sid, lane = fldmod1(@index(Local), @SIMDsize())
+
+    val = reduce_SIMD(val)
+
+    # Enkel de eerste lane van iedere SIMD unit mag de waarde naar shared memory schrijven
+    if lane == 1
+        @inbounds shared[Sid] = val
     end
-  end
+
+    @synchronize()
+
+    # de eerste 32 values worden in val gestoken, als er er minder dan 32 warps passen in 1 block
+    # dan vullen we de rest op met nullen
+    val = if @index(Local) <= fld1(@groupsize()[1], @SIMDsize())
+            @inbounds shared[lane]
+    else
+        0
+    end
+
+    # final reduce within first warp
+    if Sid == 1
+        val = reduce_SIMD(val)
+    end
+    return val
+
+end
+
+@kernel function small_reduce_kernel(R,A)
+    shared = @localmem Float64 (32,1)
+    gridIdx = @index(Local)
+    val = 0
+
+    reduce_id = gridIdx
+    while reduce_id <= length(A)
+        val += A[reduce_id]
+        reduce_id += (length(R) * @groupsize()[1])
+    end
+    
+    val = reduce_group(val)
+
+    if gridIdx == 1
+        R[1] = val
+    end 
+end
+
+@kernel function big_reduce_kernel(R,A)
+
+    gridIdx = @index(Global)
+
+    val = 0
+
+    reduce_id = gridIdx
+    while reduce_id <= length(A)
+        val += A[reduce_id]
+        reduce_id += (length(R) * @groupsize()[1])
+    end
+
+    R[gridIdx] = val
+end
+
+function mymapreducedim( R::AnyCuArray{T},
+                        A::Union{AbstractArray,Broadcast.Broadcasted}) where {T}
+
+    # Check whether R has compatible dimensions w.r.t. A for reduction
+    Base.check_reducedims(R, A)
+
+    length(A) == 0 && return R # isempty(::Broadcasted) iterates
+
+    threads = 1024
+    groups = 32
+    partial = similar(R, threads * groups)
+
+    if has_cuda_gpu()
+        if(length(R) > threads * groups )
+            big_reduce_kernel(CUDADevice(), threads)(partial, A, ndrange=threads*groups)
+        end
+
+        small_reduce_kernel(CUDADevice(), threads)(R,partial, ndrange=threads)
+    end
+    print(R[1])
+end
+
